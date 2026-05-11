5fb0869e7441daab249de0973c5f7272afb59726
mygstportalapp <119725104+mygstportalapp@users.noreply.github.com>
2026-05-11 19:27:51 +0530

    Update const.js

--- const.js
+++ const.js
@@ -1,2 +1,4 @@
-const google_id = '1021978161049-m0qcli73053amlqo773e9irqkharfved.apps.googleusercontent.com'
-const gsheet_url = 'https://script.google.com/macros/s/AKfycbwBF8Ei3H-1ZjGZXnIVVgGACkTFIlF9jj3H6hucTZydXf-thPMB1JWXO3BQoRMQIuei/exec'
+const GsheetUrl = "https://script.google.com/macros/s/AKfycbzG8v63HEzmUngcTc9u1k94FmbLipNOMKC1CeoY9bjr7mQ4gv7GN_Az6WUy4Gwl1qnNug/exec";
+const googleId = "1021978161049-m0qcli73053amlqo773e9irqkharfved.apps.googleusercontent.com";
+//Auto-Sync Interval (in milliseconds) - e.g., 60000 = 60 seconds
+const AutoSyncInterval = 60000;
